6ca652be0284194c70f61203e2afe91f67fd7037
RobinAbrahamse <rabrahamse@LAPTOP-O1589C5B.localdomain>
2020-01-31 12:43:06 -0500

    Improve linux run script

--- run.sh
+++ run.sh
@@ -2,7 +2,22 @@
 FILE=CMakeCache.txt
 if [ ! -f "$FILE" ]; then
 	cmake CMakeLists.txt
+	echo
+fi
+if [ ! -d data ]; then
+	mkdir data
+fi
+if [ ! -f data/matrix.mtx ] && [ ! -f data/vector.mtx ]; then
+	wget -O data/af_0_k101.tar.gz https://sparse.tamu.edu/MM/Schenk_AFE/af_0_k101.tar.gz
+	tar -xvzf data/af_0_k101.tar.gz -C data
+	rm -r data/af_0_k101.tar.gz
+	mv data/af_0_k101/af_0_k101.mtx data/matrix.mtx
+	mv data/af_0_k101/af_0_k101_b.mtx data/vector.mtx
+	rmdir data/af_0_k101
+	echo
 fi
 make
+echo
+echo
 ./triangular_matrix_solver ./data/matrix.mtx ./data/vector.mtx
 

7ee50e2e3f561357c5898649e472fb7bf1c8f2a6
Robin <robinabrahamse@outlook.com>
2020-01-31 17:16:49 -0500

    Add different parallel solver approaches

--- MatrixSolver.cpp
+++ MatrixSolver.cpp
@@ -21,6 +21,45 @@ using namespace std;
 * In/Out:
 * b : the right hand-side b at start and the solution x at the end.
 */
+int solve(int n, const int *col, const int *row, const double *val, double *b) {
+    if (!col || !row || !b) return 0;
+    int p, j;
+    auto d_in_degree = new int[n];
+    auto s_in_degree = new int[n];
+    auto d_sum = new double[n];
+    auto s_sum = new double[n];
+    analyse(n, col, row, d_in_degree);
+#pragma omp parallel default(none) shared(n, col, row, val, b, d_in_degree, s_in_degree, d_sum, s_sum, j) private(p)
+    {
+#pragma omp single
+        {
+            for (j = 0; j < n; j++) {
+#pragma omp task default(none) shared(col, row, val, b, d_in_degree, s_in_degree, d_sum, s_sum) private(p, j)
+                {
+                    while (s_in_degree[j] + 1 != d_in_degree[j]) {
+                        printf("Thread: %d, task: %d, expected: %d, got: %d\n", omp_get_thread_num(), j, d_in_degree[j+1], s_in_degree[j]);
+#pragma omp taskyield
+                    }      //Lock-wait section
+                    b[j] = (b[j] - d_sum[j] - s_sum[j]) / val[col[j]];
+                    double t;
+                    for (p = col[j] + 1; p < col[j + 1]; p++) {
+                        t = val[row[p]] * b[j];
+#pragma omp atomic
+                        d_sum[j] += t;                                                  //Critical section
+#pragma omp atomic
+                        d_in_degree[j]--;                                               //Lock-update section
+                    }
+                };
+            }
+        };
+    }
+    delete[] d_in_degree;
+    delete[] s_in_degree;
+    delete[] d_sum;
+    delete[] s_sum;
+    return 1;
+}
+
 //int solve(int n, const int *col, const int *row, const double *val, double *b) {
 //    if (!col || !row || !b) return 0;
 //    int p, j;
@@ -29,11 +68,13 @@ using namespace std;
 //    auto d_sum = new double[n];
 //    auto s_sum = new double[n];
 //    analyse(n, col, row, d_in_degree);
-//#pragma omp parallel default(none) shared(n, col, row, val, b, d_in_degree, s_in_degree, d_sum, s_sum) private(p, j)
+//#pragma omp parallel default(none) shared(n, col, row, val, b, d_in_degree, s_in_degree, d_sum, s_sum, j) private(p)
 //    {
 //        #pragma omp for
 //        for (j = 0; j < n; j++) {
-//            while (s_in_degree[j] + 1 != d_in_degree[j]) {/*spin thread*/}      //Lock-wait section
+//            while (s_in_degree[j] + 1 != d_in_degree[j]) {
+////                        printf("Thread: %d, task: %d, expected: %d, got: %d\n", omp_get_thread_num(), j, d_in_degree[j+1], s_in_degree[j]);
+//            }      //Lock-wait section
 //            b[j] = (b[j] - d_sum[j] - s_sum[j]) / val[col[j]];
 //            double t;
 //            for (p = col[j] + 1; p < col[j + 1]; p++) {
@@ -52,25 +93,24 @@ using namespace std;
 //    return 1;
 //}
 
-int solve(int n, const int *col, const int *row, const double *val, double *b) {
-    int p, j;
-    if (!col || !row || !b) return 0;
-    for (j = 0; j < n; j++) {
-        if (abs(b[j]) >= DBL_MIN) {
-            b[j] /= val[col[j]];
-            for (p = col[j] + 1; p < col[j + 1]; p++) {
-                b[row[p]] -= val[p] * b[j];
-            }
-        }
-    }
-    return 1;
-}
+//int solve(int n, const int *col, const int *row, const double *val, double *b) {
+//    int p, j;
+//    if (!col || !row || !b) return 0;
+//    for (j = 0; j < n; j++) {
+//        if (abs(b[j]) >= DBL_MIN) {
+//            b[j] /= val[col[j]];
+//            for (p = col[j] + 1; p < col[j + 1]; p++) {
+//                b[row[p]] -= val[p] * b[j];
+//            }
+//        }
+//    }
+//    return 1;
+//}
 
 int analyse(int n, const int *col, const int *row, int *d) {
     int j;
-#pragma omp parallel for default(none) shared(n, col, row, d) private(j)// reduction(+:d[:n])
+#pragma omp parallel for default(none) shared(n, col, row) private(j) reduction(+:d[:n])
     for (j = 0; j < col[n]; j++) {
-        #pragma omp atomic
         d[row[j]]++;
     }
     return 1;
@@ -95,7 +135,7 @@ int mult(int n, int *col, int *row, double *val, double *x, double *y) {
     for (j = 0; j < n; j++) {
         xj = x[j];
         for (p = col[j]; p < col[j + 1]; p++) {
-            #pragma omp atomic                  //Atomic instead of reduction because array size
+            #pragma omp atomic                  //Atomic instead of reduction because array size (stack overflow)
             y[row[p]] += val[p] * xj;
         }
     }
